4149b1efb05b79a2adc98424511b48b30c3c4f28
Tanmay Agrawal <tanmayagrawal244@gmail.com>
2020-05-24 05:21:30 +0530

    Question Form Added

--- app/src/main/java/com/example/amour/Login/Form.java
+++ app/src/main/java/com/example/amour/Login/Form.java
@@ -1,18 +1,17 @@
-package com.example.amour;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.amour.Login;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.amour.R;
 
 
-public class MainActivity extends AppCompatActivity {
 
-
+public class Form extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-     //setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_form);
     }
 }
