8036a98e7bf26c8a8fdbaecc3f6b1713d70cb1ee
arathod <ayushirathod44@gmail.com>
2020-06-07 23:58:18 -0700

    Extended the time for Splash screen.

--- app/src/main/java/com/example/amour/SplashActivity.java
+++ app/src/main/java/com/example/amour/SplashActivity.java
@@ -2,6 +2,8 @@ package com.example.amour;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,8 +11,22 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide(); //hide the title bar
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(2000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
     }
 }
